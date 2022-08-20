482e6d7ab9e7b6ca22eedd016036571ed9ca8761
tunmice <t.otegbayo@gmail.com>
2022-08-20 02:24:19 +0100

    Initial Commit

---
+++ Starting C++/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Starting C++
+//
+//  Created by Tunmise Otegbayo on 18/08/2022.
+//
+
+#include <iostream>
+using namespace std;
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+int main() {
+    cout << "Hello world!";
+    return 0;
+}
+
+
